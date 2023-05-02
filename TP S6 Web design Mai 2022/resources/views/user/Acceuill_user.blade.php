999b870415e047b2082abc2f2fdbcf02f70f3e9c
hardi148 <tojohajarisoa@gmail.com>
2023-05-02 03:25:47 +0100

    final farany 2

--- TP S6 Web design Mai 2022/resources/views/user/Acceuill_user.blade.php
+++ TP S6 Web design Mai 2022/resources/views/user/Acceuill_user.blade.php
@@ -117,7 +117,6 @@
         <form action="{{ url('/searchFront') }}" class="search-form" method="post">
           {{ csrf_field() }}
             <span class="icon bi-search"></span>
-            <input type="text" placeholder="Entrer un mot cle" class="form-control">
             <input type="text" name="motcle" placeholder="Entrer un mot cle" class="form-control">
             <button class="btn js-search-close"><span class="bi-x"></span></button>
           </form>
